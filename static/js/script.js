8bb7a22f249ab36cf3ac126f8340c25fe98bc424
Yoan Palacios G <44234291+PalaciosYoan@users.noreply.github.com>
2021-11-17 01:16:51 -0800

    found a way to add buttons to table

--- static/js/script.js
+++ static/js/script.js
@@ -22,15 +22,15 @@ function currClasses() {
                 "/" +
                 html[key]["capacity"] +
                 "</td></tr>";
-                //console.log(html[key]["num_enrolled"]);
+            //console.log(html[key]["num_enrolled"]);
         }
         text += "</table>";
-        
+
         document.getElementById("currentClasses").innerHTML = text;
     };
 }
 
-function potentialClasses(){
+function potentialClasses() {
     const xhttp = new XMLHttpRequest();
     const method = "GET";
     const url = "http://127.0.0.1:5000/student/potential_classes";
@@ -42,43 +42,46 @@ function potentialClasses(){
         let text =
             "<table><tr><th>Course Name</th><th>Teacher</th><th>Time</th><th>Students Enrolled</th><th>Add Course</th></tr>";
         for (key in html) {
-            
-            
-            if(html[key]["addable"] === 1)
-            {
-            let btn = document.createElement("button"); 
-            btn.innerHTML = "+";
-            document.body.appendChild(btn);
-            text +=
-                "<tr><td id='potenta'>" + 
-                html[key]["class_name"] +"</td><td>" +
-                html[key]["teacher_name"] +"</td><td>" +
-                html[key]["time"] +"</td><td>" +
-                html[key]["num_enrolled"] +"/" + html[key]["capacity"] +
-                "</td><td>" + btn +"</td></tr>";
+            if (html[key]["addable"] === 1) {
+                text +=
+                    "<tr><td id='potenta'>" +
+                    html[key]["class_name"] +
+                    "</td><td>" +
+                    html[key]["teacher_name"] +
+                    "</td><td>" +
+                    html[key]["time"] +
+                    "</td><td>" +
+                    html[key]["num_enrolled"] +
+                    "/" +
+                    html[key]["capacity"] +
+                    "</td><td>" +
+                    "<button>+</button>" +
+                    "</td></tr>";
             }
 
-            if(html[key]["addable"] === 0)
-            {
-            text +=
-                "<tr><td>" + 
-                html[key]["class_name"] +"</td><td>" +
-                html[key]["teacher_name"] +"</td><td>" +
-                html[key]["time"] +"</td><td>" +
-                html[key]["num_enrolled"] +"/" + html[key]["capacity"] +
-                "</td><td>"+ "<button type='button' disabled>+</button>"+"</td></tr>";
+            if (html[key]["addable"] === 0) {
+                text +=
+                    "<tr><td>" +
+                    html[key]["class_name"] +
+                    "</td><td>" +
+                    html[key]["teacher_name"] +
+                    "</td><td>" +
+                    html[key]["time"] +
+                    "</td><td>" +
+                    html[key]["num_enrolled"] +
+                    "/" +
+                    html[key]["capacity"] +
+                    "</td><td>" +
+                    "<button>-</button>" +
+                    "</td></tr>";
             }
-           
-            
         }
         text += "</table>";
 
         document.getElementById("potentialClasses").innerHTML = text;
     };
-
-    
-
 }
+
 function allClasses() {
     console.log("hellddddo");
 
@@ -112,7 +115,6 @@ function allClasses() {
         // document.getElementById("currentClasses").innerHTML = text;
     };
 }
-
 
 // btn.onclick = (
 //     function(entry){
